9d19d93b19930c1b8a98ba7c025e43204eb57024
Susi2002 <97386225+Susi2002@users.noreply.github.com>
2022-10-27 16:39:19 +0530

    Add files via upload
    javascript 1st class

---
+++ jquery/index.js
@@ -0,0 +1,18 @@
+alert("JAI SHREE RAM");
+//var name=prompt("Enter your name");
+//console.log(name.length);
+document.getElementsByTagName("h1")[0].style.color="white";
+document.getElementsByTagName("h1")[0].style.fontSize="3rem";
+//console.log(document.getElementsByTagName("h1").length);
+//console.log(document.getElementsByTagName("li").length);
+//document.getElementsByTagName("li")[0].style.color="white";
+//document.getElementsByTagName("title")[0].textContent="Welcome";
+//document.getElementById("para").innerHTML="<em>Welcome</em>";
+//document.querySelector("li").style.color="white";
+//document.querySelector("#l2").style.color="white";
+//document.querySelectorAll("li")[2].style.color="blue";
+//document.querySelectorAll("li")[3].style.color="blue";
+//document.querySelectorAll("li")[3].classList.add("invisible");
+//document.querySelectorAll("img).attributes.setAttributes("href"," https://cdn.iconscout.com/icon/free/png-256/astonishes-boy-1129046.png");
+
+
